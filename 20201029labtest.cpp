9e94dc9b755627bd3e00e841bd09cb2a4d4434aa
Frenky Fu <71398591+FrenkyOHOHOH@users.noreply.github.com>
2020-10-29 10:38:02 +0800

    Add files via upload

---
+++ 20201029labtest.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(){
+	cout<<"**********************************************"<<endl;
+	cout<<"*       欢迎使用学生成绩管理系统1.1          *"<<endl;
+	cout<<"*     王小明   计算机2003班 2020.10.29       *"<<endl;
+	cout<<"*     1、输入4个同学的代号和成绩             *"<<endl;
+	cout<<"*     2、显示4个同学的代号和成绩列表         *"<<endl;
+    cout<<"*     3、求4人平均分                         *"<<endl;
+    cout<<"*     4、求4人总分                           *"<<endl;
+    cout<<"*     5、查询某同学的分数                    *"<<endl;
+    cout<<"*     6、统计及格率                          *"<<endl;
+    cout<<"*     7、退出                                *"<<endl;
+    cout<<"*     请输入(1-5)选择：                      *"<<endl;
+    cout<<"*                                            *"<<endl;
+    cout<<"**********************************************"<<endl;
+    system("pause");
+	return 0;
+}
---
+++ suijishuyunsuanT1.cpp
@@ -0,0 +1,20 @@
+//a+b=?
+#include <iostream>
+#include <stdlib.h> 
+#include <time.h>  
+using namespace std;
+int main() 
+ { int a,b,c;
+ 	srand((unsigned)time(NULL));
+  a=rand();
+  b=rand();
+  cout<<a<<"+"<<b<<"=?\n";
+  cout<<"Please input your answer:";
+  cin>>c;
+  cout <<"The right answer is:"<<a<<"+"<<b<<"="<< a+b <<endl;
+  cout <<"Your answer is     :"<<a<<"+"<<b<<"="<< c <<endl;
+  if(c==a+b) cout<<"ÕýÈ·"<<endl;
+  else cout<<"´íÎó"<<endl;
+ 	return 0;
+}
+
