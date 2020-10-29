1681109db8bc507f4549c369a0f277c0abb5bd42
Frenky Fu <71398591+FrenkyOHOHOH@users.noreply.github.com>
2020-10-29 10:25:33 +0800

    Add files via upload

---
+++ huanhang.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+int main(){
+	cout<<"**********************************************"<<endl;
+	cout<<"*    欢迎使用学生成绩管理系统1.0             *"<<endl;
+	cout<<"*       傅学瑜   计算机2003班 2020.10.8      *"<<endl;
+	cout<<"*     1、输入语、数、英三门课程成绩          *"<<endl;
+	cout<<"*     2、查询程序                            *"<<endl;
+    cout<<"*     3、求平均分；                          *"<<endl;
+    cout<<"*     4、退出                                *"<<endl;
+    cout<<"*   请输入(1-4)选择：                        *"<<endl;
+    cout<<"*                                            *"<<endl;
+    cout<<"**********************************************"<<endl;
+    system("pause");
+	return 0;
+}
---
+++ welcome1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdlib.h> 
+using namespace std;
+int main()
+{
+cout<<"Welcome to Guangdong university of Foreign Studies .\n";
+system("pause");
+return 0;
+}
